177be8bd976aba53a3320f1ddedd61286d44a7e1
Matouuuu <148874093+Matouuuu@users.noreply.github.com>
2026-03-10 10:11:10 +0100

    Test main enregistrement JSON

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <QApplication>
 #include <QDir>
 #include <iostream>
-
+#include "enseignant.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
@@ -16,8 +16,25 @@ int main(int argc, char *argv[])
 
     std::cout << "Data : " << data_path.toStdString() << std::endl;
 
+    // 1. Création de l'enseignant
+    Enseignant e("zidane", "zinedine");
+
+    // 2. Préparation du chemin complet du fichier
+    QString filepath = data_path + "enseignants.json";
+
+    // 3. Création du vecteur et ajout de l'enseignant
+    std::vector<Enseignant> listeEnseignants;
+    listeEnseignants.push_back(e);
+
+    Enseignant e2("trenchant","vincent");
+    listeEnseignants.push_back(e2);
+
+    // 4. Sauvegarde dans le fichier JSON
+    Enseignant::writeToJSON(listeEnseignants, filepath);
+    std::cout << "Enseignant sauvegarde dans : " << filepath.toStdString() << std::endl;
     MainWindow w(data_path);
     w.show();
+
 
     return a.exec();
 }
