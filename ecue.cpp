97d650c5b95a7a3992dd04afc7b4f2b84fa7c681
AxelB-rgb <axel.brilhac@gmail.com>
2026-03-11 15:08:10 +0100

    fin ecue

--- ecue.cpp
+++ ecue.cpp
@@ -75,8 +75,8 @@ std::string Ecue::to_string() const
     switch(typeCours)
     {
         case eTypeCours::CM:
-            res = "Enseignant : " + enseignant.toString() + ", Groupe étudiant : " + groupeEtudiant.toString() _
-                    ", Type de cours : CM, Heure Total : " + std::to_string(heureTotal) _
+            res = "Enseignant : " + enseignant.toString() + ", Groupe étudiant : " + groupeEtudiant.toString() + _
+                  ", Type de cours : CM, Heure Total : " + std::to_string(heureTotal) _
                     ", Heure restante : " + std::to_string(heurerestante);
         case eTypeCours::TD:
             res = "Enseignant : " + enseignant.toString() + ", Groupe étudiant : " + groupeEtudiant.toString() _
@@ -144,21 +144,106 @@ QJsonObject Ecue::toJSON(void) const
 //a finir
 Ecue Ecue::fromJSON(QJsonObject json){
     QJsonObject::iterator it;
+    QJsonObject obj;
 
     it = json.find("Enseignant");
-    if (it==json.end()) { qDebug() << "Le JSON ne contient pas de 'Enseignant";}
-    if (it.value().isNull() ) { qDebug() << "Le 'Enseignant' est null" ;}
-    std::string enseignant = it.value().toString().toStdString() ;
+    if (it==json.end()) { qDebug() << "Le JSON ne contient pas de 'Enseignant'";}
+    if (it.value().isNull() || it.value().isObject() ) { qDebug() << "Le 'Enseignant' est null" ;}
+
+    QJsonObject JOEnseignant = it.value().toObject();
+
+    it = JOEnseignant.find("prenom");
+    if (it==JOEnseignant.end()) { qDebug() << "Le JSON Enseignant ne contient pas de 'prenom";}
+    if (it.value().isNull() ) { qDebug() << "Le 'prenom' de Enseignant est null" ;}
+    std::string sPrenomEnseignant = it.value().toString().toStdString() ;
+
+    it = JOEnseignant.find("nom");
+    if (it==JOEnseignant.end()) { qDebug() << "Le JSON Enseignant ne contient pas de 'nom";}
+    if (it.value().isNull() ) { qDebug() << "Le 'nom' de Enseignant est null" ;}
+    std::string sNomEnseignant = it.value().toString().toStdString() ;
+
+    it = JOEnseignant.find("mail");
+    if (it==JOEnseignant.end() ) { qDebug() << "Le JSON Enseignant ne contient pas de 'mail";}
+    if (it.value().isNull() ) { qDebug() << "Le 'mail' de Enseignant est null" ;}
+    std::string sMailEnseignant = it.value().toString().toStdString() ;
 
     it = json.find("GroupeEtudiant");
-    if (it==json.end()) { qDebug() << "Le JSON ne contient pas de 'nom";}
-    if (it.value().isNull() ) { qDebug() << "Le 'nom' est null" ;}
-    std::string nom = it.value().toString().toStdString() ;
+    if (it==json.end()) { qDebug() << "Le JSON ne contient pas de 'GroupeEtudiant'";}
+    if (it.value().isNull() || it.value().isObject()) { qDebug() << "Le 'GroupeEtudiant' est null" ;}
 
-    it = json.find("mail");
-    if (it==json.end() ) { qDebug() << "Le JSON ne contient pas de 'mail";}
-    if (it.value().isNull() ) { qDebug() << "Le 'mail' est null" ;}
-    std::string mail = it.value().toString().toStdString() ;
+    QJsonObject OJGroupeEtudiant = it.value().toObject();
 
-    return Enseignant(prenom, nom, mail);
+    it = OJGroupeEtudiant.find("nom");
+    if (it==OJGroupeEtudiant.end()) { qDebug() << "Le JSON GroupeEtudiant ne contient pas de 'nom'";}
+    if (it.value().isNull() ) { qDebug() << "Le 'nom' de GroupeEtudiant est null" ;}
+    std::string sNomGroupeEtudiant = it.value().toString().toStdString() ;
+
+    it = json.find("TypeCours");
+    if (it==json.end() ) { qDebug() << "Le JSON ne contient pas de 'TypeCours'";}
+    if (it.value().isNull() ) { qDebug() << "Le 'TypeCours' est null" ;}
+    std::string sTypeCours = it.value().toString().toStdString();
+
+    it = json.find("HeureTotal");
+    if (it==json.end() ) { qDebug() << "Le JSON ne contient pas de 'HeureTotal'";}
+    if (it.value().isNull() ) { qDebug() << "Le 'HeureTotal' est null" ;}
+    int iHeureTotal = it.value().toInt();
+
+    it = json.find("HeureRestante");
+    if (it==json.end() ) { qDebug() << "Le JSON ne contient pas de 'HeureRestante'";}
+    if (it.value().isNull() ) { qDebug() << "Le 'HeureRestante' est null" ;}
+    int iHeureRestante = it.value().toInt();
+
+    if(sTypeCours == "CM"){
+        return Ecue(Enseignant(sPrenomEnseignant, sNomEnseignant, sMailEnseignant), GroupeEtudiant(sNomGroupeEtudiant), eTypeCours::CM, iHeureTotal, iHeureRestante);
+    }else if(sTypeCours == "TD"){
+        return Ecue(Enseignant(sPrenomEnseignant, sNomEnseignant, sMailEnseignant), GroupeEtudiant(sNomGroupeEtudiant), eTypeCours::TD, iHeureTotal, iHeureRestante);
+    }else if(sTypeCours == "TP_INFO"){
+        return Ecue(Enseignant(sPrenomEnseignant, sNomEnseignant, sMailEnseignant), GroupeEtudiant(sNomGroupeEtudiant), eTypeCours::TP_INFO, iHeureTotal, iHeureRestante);
+    }else if(sTypeCours == "TP_ELEC"){
+        return Ecue(Enseignant(sPrenomEnseignant, sNomEnseignant, sMailEnseignant), GroupeEtudiant(sNomGroupeEtudiant), eTypeCours::TP_ELEC, iHeureTotal, iHeureRestante);
+    }else if(sTypeCours == "EXAMEN_EN_SALE"){
+        return Ecue(Enseignant(sPrenomEnseignant, sNomEnseignant, sMailEnseignant), GroupeEtudiant(sNomGroupeEtudiant), eTypeCours::EXAMEN_EN_SALE, iHeureTotal, iHeureRestante);
+    }else if(sTypeCours == "EXAMEN_INFO"){
+        return Ecue(Enseignant(sPrenomEnseignant, sNomEnseignant, sMailEnseignant), GroupeEtudiant(sNomGroupeEtudiant), eTypeCours::EXAMEN_INFO, iHeureTotal, iHeureRestante);
+    }else if(sTypeCours == "EXAMEN_ELEC"){
+        return Ecue(Enseignant(sPrenomEnseignant, sNomEnseignant, sMailEnseignant), GroupeEtudiant(sNomGroupeEtudiant), eTypeCours::EXAMEN_ELEC, iHeureTotal, iHeureRestante);
+    }
+}
+
+void Ecue::writeToJSON(std::vector<Ecue> vector, QString filepath){
+    QFile file (filepath);
+    if (!file.open(QIODevice::WriteOnly | QIODevice::Text)){
+        qDebug() << "Erreur ouverture de";
+        qDebug() << filepath.toStdString();
+    }
+    QJsonArray tab;
+    for (auto it =vector.begin(); it != vector.end(); it++)
+    {
+        tab.append(it->toJSON());
+    }
+    file.write(QJsonDocument(tab).toJson(QJsonDocument::Indented));
+    file.close();
+}
+
+std::vector<Ecue> Ecue::readFromJSON(QString filepath){
+    QFile file (filepath);
+    if (!file.open(QIODevice::ReadOnly)){
+        qDebug() << "Erreur ouverture de";
+        qDebug() << filepath.toStdString();
+        return std::vector<Ecue>();
+    }
+    QByteArray data = file.readAll();
+    file.close();
+    QJsonDocument doc = QJsonDocument::fromJson(data);
+    if(!doc.isArray()){
+        qDebug() << "Erreur ouverture de";
+        qDebug() << filepath;
+        return std::vector<Ecue>();
+    }
+    QJsonArray tab =doc.array();
+    std::vector<Ecue> res;
+    for (auto it = tab.begin(); it != tab.end();it++){
+        res.push_back(Ecue::fromJSON((*it).toObject()));
+    }
+    return res;
 }
