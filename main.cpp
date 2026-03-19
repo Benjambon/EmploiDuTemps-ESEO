9d565604476e338cb43245abf872b640ac528762
Matouuuu <148874093+Matouuuu@users.noreply.github.com>
2026-03-19 11:41:20 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,9 @@
 #include "mainwindow.h"
-#include "ecue.h"
-
-#include <iostream>
-#include <QString>
-#include <QCoreApplication>
 #include <QApplication>
 #include <QDir>
+#include <iostream>
+#include "enseignant.h"
+#include "salle.h"
 
 int main(int argc, char *argv[])
 {
@@ -21,25 +19,33 @@ int main(int argc, char *argv[])
     // 1. Création de l'enseignant
     Enseignant e("zidane", "zinedine");
 
-    GroupeEtudiant gE("E4e");
-
-    Ecue ecue(e, gE, eTypeCours::CM, 14, 0);
     // 2. Préparation du chemin complet du fichier
-    QString filepath = data_path + "ecue.json";
+    QString filepath = data_path + "enseignants.json";
 
     // 3. Création du vecteur et ajout de l'enseignant
-    std::vector<Ecue> listeEcue;
-    listeEcue.push_back(ecue);
+    std::vector<Enseignant> listeEnseignants;
+    listeEnseignants.push_back(e);
 
-    //Enseignant e2("trenchant","vincent");
-    //listeEcue.push_back(e2);
+    Enseignant e2("trenchant","vincent");
+    listeEnseignants.push_back(e2);
 
     // 4. Sauvegarde dans le fichier JSON
-    Ecue::writeToJSON(listeEcue, filepath);
-    std::cout << "Ecue sauvegarde dans : " << filepath.toStdString() << std::endl;
+    Enseignant::writeToJSON(listeEnseignants, filepath);
+    std::cout << "Enseignant sauvegarde dans : " << filepath.toStdString() << std::endl;
+
+    // Ajout du test pour la classe Salle
+    Salle salleInfo("001", TypeSalle::INFORMATIQUE);
+    std::cout << "Affichage de la salle : " << salleInfo.toString() << std::endl;
+
+    QString filepathSalles = data_path + "salles.json";
+    std::vector<Salle> listeSalles;
+    listeSalles.push_back(salleInfo);
+
+    Salle::writeToJSON(listeSalles, filepathSalles);
+    std::cout << "Salle sauvegardee dans : " << filepathSalles.toStdString() << std::endl;
+
     MainWindow w(data_path);
     w.show();
 
-
-    return 0 ;
+    return a.exec();
 }
