3c64aa4cf20226fbef788f6ae32a30eeba780e4d
anto7176 <antoine.vadot71@gmail.com>
2026-03-23 10:58:03 +0100

    Add controllers for teacher/group/room and tests
    Add controller classes for Enseignant, GroupeEtudiant and Salle (controleur_enseigant, controleur_groupeetudiant, controleur_salle) with basic validation wrappers. Update EmploiDuTempsESEO.pro to include the new source and header files. Update main.cpp to include the new controllers and add console tests that exercise TestNom/TestPrenom/TestMail/TestNomGroupe/TestSalle methods, printing validation results. Controllers construct model objects and call existing is*Valid() methods, returning human-readable French messages for success/failure.

---
+++ controleur_groupeetudiant.h
@@ -0,0 +1,15 @@
+#ifndef CONTROLEUR_GROUPEETUDIANT_H
+#define CONTROLEUR_GROUPEETUDIANT_H
+
+#include <string>
+
+class Controleur_groupeetudiant
+{
+public:
+
+    Controleur_groupeetudiant();
+
+    static std::string TestNomGroupe(std::string nom);
+};
+
+#endif // CONTROLEUR_GROUPEETUDIANT_H
--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@
 #include "enseignant.h"
 #include "salle.h"
 
+#include "controleur_groupeetudiant.h"
+#include "controleur_enseignant.h"
+#include "controleur_salle.h"
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
@@ -43,6 +47,59 @@ int main(int argc, char *argv[])
 
     Salle::writeToJSON(listeSalles, filepathSalles);
     std::cout << "Salle sauvegardee dans : " << filepathSalles.toStdString() << std::endl;
+
+
+    // TEST Controleurs
+
+    /* tester si le resultat est bein celui quon souhaite */
+
+    std::string Nom = "Vadot";
+    std::string resNom = Controleur_enseignant::TestNomEnseignant(Nom);
+    std::cout << resNom << std::endl;
+
+
+    std::string Nom2 = "";
+    std::string resNom2 = Controleur_enseignant::TestNomEnseignant(Nom2);
+    std::cout << resNom2 << std::endl;
+
+    std::string Prenom = "Antoine";
+    std::string resPrenom = Controleur_enseignant::TestPrenomEnseignant(Prenom);
+    std::cout << resPrenom << std::endl;
+
+    std::string Prenom2 = "";
+    std::string resPrenom2 = Controleur_enseignant::TestPrenomEnseignant(Prenom2);
+    std::cout << resPrenom2 << std::endl;
+
+    std::string Mail = "antoine.vadot@gmail.com";
+    std::string resMail = Controleur_enseignant::TestMailEnseignant(Mail);
+    std::cout << resMail << std::endl;
+
+    std::string Mail2 = "antoine.vadot.com";
+    std::string resMail2 = Controleur_enseignant::TestMailEnseignant(Mail2);
+    std::cout << resMail2 << std::endl;
+
+
+    std::string nomGroupe1 = "Groupe A";
+    std::string resGroupe1 = Controleur_groupeetudiant::TestNomGroupe(nomGroupe1);
+    std::cout << resGroupe1 << std::endl; // Affiche "Nom de groupe OK"
+
+    std::string nomGroupe2 = "";
+    std::string resGroupe2 = Controleur_groupeetudiant::TestNomGroupe(nomGroupe2);
+    std::cout << resGroupe2 << std::endl; // Affiche "Le nom du groupe ne peut pas etre vide !"
+
+
+    // Test d'une salle valide
+    std::string resSalle1 = Controleur_salle::TestSalle("104", TypeSalle::INFORMATIQUE);
+    std::cout << resSalle1 << std::endl; // Affiche "Salle OK"
+
+    // Test d'une salle invalide (lettres à la place des chiffres)
+    std::string resSalle2 = Controleur_salle::TestSalle("A12", TypeSalle::SALLE);
+    std::cout << resSalle2 << std::endl; // Affiche l'erreur
+
+    // Test d'une salle invalide (trop de chiffres)
+    std::string resSalle3 = Controleur_salle::TestSalle("1004", TypeSalle::ELECTRONIQUE);
+    std::cout << resSalle3 << std::endl; // Affiche l'erreur
+
 
     MainWindow w(data_path);
     w.show();
